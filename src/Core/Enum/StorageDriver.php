fde31b6764af346521fcad1d2d1345349fd9f51e
celionatti <amisuusman@gmail.com>
2023-06-27 02:44:38 +0100

    Home Work -1

---
+++ src/Core/Enum/StorageDriver.php
@@ -0,0 +1,10 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace Bootles\Core\Enum;
+
+enum StorageDriver
+{
+    case Local;
+}
---
+++ src/configs/path_constants.php
@@ -0,0 +1,9 @@
+<?php
+
+declare(strict_types = 1);
+
+const APP_PATH     = __DIR__ . '/../src';
+const TEMPLATE_PATH    = __DIR__ . '/../templates';
+const STORAGE_PATH = __DIR__ . '/../storage';
+const CONFIG_PATH  = __DIR__ . '/../src/configs';
+const BUILD_PATH   = __DIR__ . '/../public/build';
